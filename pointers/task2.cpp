4834d6407ce0747839ec77cbef6531f293686b54
Lukasz Fabia <114498264+lukaszfabia@users.noreply.github.com>
2023-08-13 18:53:07 +0000

    new task and fix task1

---
+++ pointers/task2.cpp
@@ -0,0 +1,32 @@
+// u have 3 numbers and sort by using pointers
+#include <iostream>
+
+using namespace std;
+
+void sort(int *x, int *y, int *z); // send originals to function
+
+int main(int argc, char const *argv[])
+{
+    int x, y, z;
+    cin >> x >> y >> z;
+    sort(&x, &y, &z); // giving adresses to variables 
+    cout << x << " " << y << " " << z << endl;
+
+    return 0;
+}
+
+void sort(int *x, int *y, int *z)
+{
+    if (*x > *y)
+    {
+        swap(*x, *y);
+    }
+    if (*x > *z)
+    {
+        swap(*x, *z);
+    }
+    if (*y > *z)
+    {
+        swap(*y, *z);
+    }
+}
