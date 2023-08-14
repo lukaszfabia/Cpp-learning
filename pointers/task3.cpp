0c01d4d38668d236ac53f3560a628c5e1de66984
LukaszAcer <ufabia03@gmail.com>
2023-08-14 16:59:22 +0200

    task3 added new functions

--- pointers/task3.cpp
+++ pointers/task3.cpp
@@ -1,3 +1,6 @@
+// note: when u work on array u dont need to use &arr[i] to get address of element but when u use
+// non array vars you have to "&" when you giving variable to function when there is declarated pointer in argument of function
+
 #include <iostream>
 #include <cstdlib>
 
@@ -5,48 +8,111 @@ using namespace std;
 
 int len;
 void randomize(int *arr); // set random numbers to array
-void iter(int *arr); // iterate through array (print all elements
+void iter(int *arr);      // iterate through array (print all elements)
 int sum(int *arr);
 float average(int *arr);
+void reverse(int *arr);
+bool add(int *arr, int element);
+bool add(int *arr, int element, int index); // add element to array at index (index must be < len
 
 int main()
 {
-    int *arr = new int [len];
-    cout << "Enter length of array: ";
+    int *arr = new int[len];
+    printf("Enter length of array: ");
     cin >> len;
     randomize(arr);
-    cout << "Sum of array: " << sum(arr) << endl;
-    cout << "Average of array: " << average(arr) << endl;
+    printf("Sum of array: %d\n", sum(arr));
+    printf("Average of array: %.2f\n", average(arr));
     iter(arr);
-    delete [] arr;
+    printf("\n");
+    reverse(arr);
+    iter(arr);
+    add(arr, 5);
+    iter(arr);
+    add(arr, -1, 2);
+    add(arr, 10, len);
+    iter(arr);
+
+    delete[] arr;
 
     return 0;
 }
 
-void randomize(int *arr){
+void randomize(int *arr)
+{
     srand(time(NULL));
-    for (int i = 0; i<len; i++){
-        *arr = rand() % 100+1;
+    for (int i = 0; i < len; i++)
+    {
+        *arr = rand() % 100 + 1;
         arr++;
     }
 }
 
-int sum(int *arr){
+int sum(int *arr)
+{
     int sum = 0;
-    for (int i = 0; i<len; i++){
+    for (int i = 0; i < len; i++)
+    {
         sum += *arr;
         arr++;
     }
     return sum;
 }
 
-float average(int *arr){
-    return (float)sum(arr)/len;
+float average(int *arr)
+{
+    return (float)sum(arr) / len;
 }
 
-void iter(int *arr){
-    for (int i = 0; i<len; i++){
+void iter(int *arr)
+{
+    for (int i = 0; i < len; i++)
+    {
         printf("%d ", *arr);
         arr++;
     }
+    printf("\n");
+}
+
+void reverse(int *arr)
+{
+    // O(len/2)
+    int *start = arr;
+    int *end = arr+len-1;
+    while (start<end)
+    {
+        swap(*start, *end);
+        start++, end--;
+    }
+}
+
+bool add(int *arr, int element){
+    int *end = len+arr;
+    len++;
+    *end = element;
+
+    return true;
+}
+
+bool add(int *arr, int element, int index){
+    if (index>len){
+        return false;
+    }
+    else{
+        int *end = len+arr;
+        len++;
+        while (end>arr+index)
+        {
+            *end = *(end-1);
+            end--;
+        }
+        *end = element;
+        return true;
+    }
+
+
+
+
+
+
 }
