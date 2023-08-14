ef12455d8be145b07114d0d2c45472164f29ebe9
LukaszAcer <ufabia03@gmail.com>
2023-08-14 17:18:03 +0200

    added task3.h

--- pointers/task3.cpp
+++ pointers/task3.cpp
@@ -3,17 +3,11 @@
 
 #include <iostream>
 #include <cstdlib>
+#include "task3.h"
 
 using namespace std;
 
 int len;
-void randomize(int *arr); // set random numbers to array
-void iter(int *arr);      // iterate through array (print all elements)
-int sum(int *arr);
-float average(int *arr);
-void reverse(int *arr);
-bool add(int *arr, int element);
-bool add(int *arr, int element, int index); // add element to array at index (index must be < len
 
 int main()
 {
